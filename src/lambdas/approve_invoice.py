c4b93b51fe115173225d8d482904859873468650
dj2802 <dhanushraj28022007@gmail.com>
2025-12-29 10:48:27 +0530

    Implement lambda_handler for invoice approval

--- src/lambdas/approve_invoice.py
+++ src/lambdas/approve_invoice.py
@@ -1,1 +1,11 @@
+def lambda_handler(event, context):
+    invoice_id = event.get("invoiceId")
+    amount = event.get("amount")
+    vendor = event.get("vendor")
 
+    return {
+        "status": "APPROVED",
+        "invoiceId": invoice_id,
+        "amount": amount,
+        "vendor": vendor
+    }
